0545da437b369b956b4d8a7a5262bf2926b6e3c4
Acre <10746503+IlionAcre@users.noreply.github.com>
2025-08-20 09:58:10 -0500

    Third class, functions

---
+++ 08/19 - If, else, else if/script.js
@@ -0,0 +1,100 @@
+// let year = '18';
+// let year2 = '10';
+// let resta = '10' - '5' + 3;
+// year = Number(year);
+
+// console.log(year);
+// console.log(typeof year);
+
+// let nombre = Number('Jonas');
+// console.log(nombre);
+// console.log(typeof NaN);
+
+// console.log(Boolean(year));
+// console.log(Boolean(year2));
+
+// console.log(Boolean(nombre));
+
+
+// console.log(Boolean(0));
+// console.log(Boolean(''));
+// console.log(Boolean(NaN));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+
+// console.log(Boolean({}));
+
+// let comida = prompt('Que quieres de comer');
+// if (comida) {
+//   console.log('comida')
+// } else {
+//   console.log('No elegiste nada');
+// }
+
+// if (!comida) {
+//   console.log('No elegiste nada')
+// } else {
+//   console.log(comida);
+// }
+
+
+// && = and, || = or, ! = not
+
+// let edad = prompt('Indica tu edad');
+// let bebida = prompt('Que quieres tomar?')
+
+// if (edad < 18 && bebida === 'cerveza') {
+//   console.log('No puedes tomar cerveza')
+// } else if (edad < 3 || bebida === 'mate') {
+//   console.log('No se puede procesar la orden')
+// } else {
+//   console.log('Anotado')
+// }
+
+// let diaSemana = prompt('Que dia es?')
+
+// switch(diaSemana) {
+//   case 'lunes':
+//     console.log('Papaya');
+//     console.log('Papaya');
+//     console.log('Papaya');
+//     console.log('Papaya');
+//     break;
+//   case 'martes':
+//   case 'jueves':
+//     console.log('mango');
+//     break;
+//   case 'miercoles':
+//   case 'viernes':
+//     console.log('fresa');
+//     break;
+//   default:
+//     console.log('No abrimos');
+//     break;
+// }
+
+// Operador ternario condicion ? ejecucion para if : ejecucion para else
+
+// let edad = prompt('edad');
+// edad = Number(edad);
+
+// let bebida = edad >= 18 ? 'ron' : 'jugo';
+
+// console.log(`Tu bebida es ${bebida}`);
+
+// let nota = 43;
+// let mensaje = nota >= 90 ? "Excelente" 
+//               : nota >= 60 ? "Aprobado" 
+//               : "Reprobado"; 
+// console.log(mensaje);
+
+
+// if (nota >= 90) {
+//   mensaje = "Excelente";
+// } else {
+//   if (nota >= 60) {
+//     mensaje = "Aprobado";
+//   } else {
+//     mensaje = "Reprobado";
+//   }
+// }
